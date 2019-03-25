aec8af5faf7878135f68cd002da26b105c85bb36
Lucasfb <Lucas1214fb@gmail.com>
2019-03-25 12:34:07 -0300

    Ajustes em nomes de macros para facilitar compreensao

---
+++ controle_IR_demonstracao/Debug/src/IR_NEC.d
@@ -0,0 +1,31 @@
+# FIXED
+
+src/IR_NEC.obj: ../src/IR_NEC.c
+src/IR_NEC.obj: D:/OneDrive/Git\ Repositories/transmissor_IR/controle_IR_demonstracao/inc/IR_NEC.h
+src/IR_NEC.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/stdint.h
+src/IR_NEC.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/stdint.h
+src/IR_NEC.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/cdefs.h
+src/IR_NEC.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/_types.h
+src/IR_NEC.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/machine/_types.h
+src/IR_NEC.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/machine/_stdint.h
+src/IR_NEC.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/_stdint.h
+src/IR_NEC.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430.h
+src/IR_NEC.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430g2452.h
+src/IR_NEC.obj: C:/ti/ccsv8/ccs_base/msp430/include/in430.h
+src/IR_NEC.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/intrinsics.h
+src/IR_NEC.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/intrinsics_legacy_undefs.h
+
+../src/IR_NEC.c: 
+D:/OneDrive/Git\ Repositories/transmissor_IR/controle_IR_demonstracao/inc/IR_NEC.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/msp430g2452.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/intrinsics.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/intrinsics_legacy_undefs.h: 
